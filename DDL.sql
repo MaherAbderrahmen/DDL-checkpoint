24ff20dc9c40f2cbedf26ada079853f904dd915b
MaherAbderrahmen <maher.abderrahmen@gmail.com>
2020-11-14 22:07:49 +0100

    second commit

--- DDL.sql
+++ DDL.sql
@@ -1,10 +1,12 @@
+/*DESCRIBE Product*/
+
 CREATE TABLE Product(
     Product_id VARCHAR(20) CONSTRAINT pk_product PRIMARY KEY,
     product_name VARCHAR(20) CONSTRAINT cons_product NOT NULL,
     Price NUMBER CONSTRAINT check_price CHECK (Price>0).
 );
-DESCRIBE Product;
 
+/*DESCRIBE Orders*/
 CREATE TABLE Orders(
     Costumer_id VARCHAR(20) CONSTRAINT pk_costumer PRIMARY KEY,
     Product_id VARCHAR(20),
@@ -13,14 +15,14 @@ CREATE TABLE Orders(
     CONSTRAINT fk_costumer FOREIGN KEY (Costumer_id) REFERENCES Costumers (Costumer_id),
     CONSTRAINT fk_product FOREIGN Key (Product_id) REFERENCES Producs (Product_id),
 );
-DESCRIBE Orders;
 
+/*DESCRIBE Costumers*/
 CREATE TABLE Costumers(
     Costumer_id VARCHAR(20),
     Costumer_name VARCHAR(20) CONSTRAINT cons_costumer NOT NULL.
     Costumer_tel NUMBER,
 );
-DESCRIBE Costumers;
+
 
 Alter TABLE Product ADD Category VARCHAR(20);
 Alter TABLE Orders ADD OrderDate DATE(SYSDATE);
